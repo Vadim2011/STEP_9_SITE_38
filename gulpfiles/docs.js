6b86e8ab6cd83046139c3b44adb01a6f8ad03e2a
asdfvadim <aaaaa@mmm.ru>
2024-01-11 02:53:10 +0800

    first commit

--- gulpfiles/docs.js
+++ gulpfiles/docs.js
@@ -200,17 +200,17 @@ export const svgSpriteProd = () => {
         pretty: true,
       },
     }))
-    .pipe(cheerio({
-      run: function ($) {
-        $('[fill]').removeAttr('fill');
-        $('[stroke]').removeAttr('stroke');
-        $('[style]').removeAttr('style');
-      },
-      parserOptions: {
-        xmlMode: true
-      },
-    }))
-    .pipe(replace('&gt;', '>'))
+    // .pipe(cheerio({
+    //   run: function ($) {
+    //     $('[fill]').removeAttr('fill');
+    //     $('[stroke]').removeAttr('stroke');
+    //     $('[style]').removeAttr('style');
+    //   },
+    //   parserOptions: {
+    //     xmlMode: true
+    //   },
+    // }))
+    // .pipe(replace('&gt;', '>'))
     .pipe(svgSprite({
       mode: {
         symbol: {
